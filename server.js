d3a29fd11283964f9a9a443660ddbde4a0cff9e7
Mahyar Sepehr <mahyar.sepehr@gmail.com>
2019-01-17 15:29:32 +0100

    modified server

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
-let counter = 0
-let host = '0.0.0.0'
-// PORT PCF
-let port = process.env.PORT || 8080
-console.log('listening on interface', host, 'on port', port)
-require('http').createServer((req, res) => {
-  counter += 1
+// get port number from cloud platform
+const port = process.env.PORT || 8080
 
-  let date = new Date().toISOString()
-  console.log(date, req.method, req.url)
+const host = '0.0.0.0'
 
-  res.setHeader('Content-Type', 'application/json')
-  if (req.url === '/counter') {
-    res.write(JSON.stringify({counter}))
-  } else if (req.url === '/health') { // health-check-http-endpoint
-  } else {
-    console.error(date, 'file not found')
-    res.writeHead(404)
-  }
-  res.end()
-}).listen(port, host)
+let requests = 0
+
+function metrics() {
+    return {
+        name: "requests",
+        measurements: [
+            { statistics: "Count", value: requests }
+        ],
+        availableTags: []
+    }
+}
+
+function handler(req, res) {
+    requests += 1
+
+    let date = new Date().toISOString()
+    console.log(date, req.method, req.url)
+
+    res.setHeader('Content-Type', 'application/json')
+    if (req.url === '/metrics' ||
+        req.url === '/actuator/metrics') {
+        res.write(JSON.stringify({ names: [ "requests" ] }))
+    else if (req.url === 'actuator/metrics/requests') {
+        res.write(JSON.stringify(metrics()))
+    } else if (req.url === '/health') {
+        // health-check-http-endpoint
+    } else {
+        console.error(date, 'file not found')
+        res.writeHead(404)
+    }
+    res.end()
+}
+
+console.log('listening on interface', host, 'on port', port, '...')
+require('http').createServer(handler).listen(port, host)
